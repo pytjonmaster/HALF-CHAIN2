aa741d63983a41e5a293cccbbf184414b1b79ef9
pytjonmaster <139779813+pytjonmaster@users.noreply.github.com>
2025-06-13 13:08:00 +0400

    ready to production

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { setupSecurityMiddleware } from './config/security.js';
 import authRoutes from './routes/auth.js';
+import path from 'path';
 
 // Initialize environment variables
 dotenv.config();
 
 // Initialize express app
 const app = express();
-
+const __dirname = path.resolve()
 // Body parser
 app.use(express.json());
 
@@ -23,7 +24,13 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 // Start server
 const PORT = process.env.PORT || 9876;
 app.listen(PORT, () => {
